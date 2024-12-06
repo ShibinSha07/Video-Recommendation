f43e95130f5db0f3ad99a3534c556fd1ea582781
shibinsha <ahammedshibinsha07@gmail.com>
2024-12-06 19:48:59 +0530

    remove learn folder

--- main.py
+++ main.py
@@ -27,6 +27,17 @@ items={
 def index()->dict[str,dict[int,Item]]:
     return {"items":items}
 
+# @app.get("/items/{item_id}")
+# def query_item_by_id(item_id:int)->Item:
+#     if item_id not in Item:
+#         raise HTTPException(
+#             status_code=404,details=f"Item with{item_id=} does not exist"
+#         )
+#     return items[item_id]
+
+
+
+
 
 
 
